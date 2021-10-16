ac5e7d578679e35157f6ecc9514ae555ddaccf30
Miguel <migzhasbite@gmail.com>
2021-10-16 18:39:12 -0400

    Done with form functionality. Moving on to styling both pages.

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,3 +1,12 @@
+// You must create the list of concerts using JavaScript DOM manipulation / flexbox layout.
+// You must have an array in JavaScript with all of concerts data and render the concerts HTML dynamically using the array data. It’s up to your discretion to decide on properties you create to represent all of the individual concert data.
+// No template literals should be used. All dynamic HTML should be added to DOM via DOM Methods for individual elements. Avoid bulk assigning stringified HTML using innerHTML
+// Utilize your knowledge of JavaScript DOM Manipulation and built in functions to create all the content between the hero image and the footer, as well as create your own functions as necessary. There should be no need to have any shows content housed within your html file for this section.
+// The individual rows of the Shows table will have different styling applied depending on the state of the table row. Utilize your knowledge of both JavaScript and Sass to accomplish this.
+// The individual rows of the Shows table need to have a hover state applied to them when a cursor is hovering over the table row, as per style guide. This can be done by utilizing a pseudo class within your Sass.
+// Additionally, clicking on an individual row should make that row "selected" or "active", applying a modifier CSS class via JavaScript. Utilize your knowledge of both JavaScript and Sass to accomplish this.
+// Your styling will still be applied through your Sass files. Do not use the built in JavaScript DOM style method.
+
 const showsArr = [
 	{
 		date: "Mon Sept 06 2021",
