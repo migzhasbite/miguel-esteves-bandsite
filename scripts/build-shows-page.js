9f9ea68b4667cb322d8a23bc908fb18ae58dd657
Miguel <migzhasbite@gmail.com>
2021-10-16 11:07:09 -0400

    Able to arrays provided on copy deck onto site. working on button functionality and styling after.

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,22 +1,95 @@
-// const comments = [
-// 	{
-// 		name: "Connor Walton",
-// 		date: "02/17/2021",
-// 		content:
-// 			"This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-// 	},
-// 	{
-// 		name: "Emilie Beach",
-// 		date: "01/09/2021",
-// 		content:
-// 			"I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-// 	},
-// 	{
-// 		name: "Miles Acosta",
-// 		date: "12/20/2020",
-// 		content:
-// 			"I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-// 	},
-// ];
+const showsArr = [
+	{
+		date: "Mon Sept 06 2021",
+		venue: "Ronald Lane",
+		location: "San Francisco, CA",
+	},
+	{
+		date: "Tue Sept 21 2021",
+		venue: "Pier 3 East",
+		location: "San Francisco, CA",
+	},
+	{
+		date: "Fri Oct 15 2021",
+		venue: "View Lounge",
+		location: "San Francisco, CA",
+	},
+	{
+		date: "Sat Nov 06 2021",
+		venue: "Hyatt Agency",
+		location: "San Francisco, CA",
+	},
+	{
+		date: "Fri Nov 26 2021",
+		venue: "Moscow Center",
+		location: "San Francisco, CA",
+	},
+	{
+		date: "Wed Dec 15 2021",
+		venue: "Press Club",
+		location: "San Francisco, CA",
+	},
+];
 
-// console.log(comments);
+const showsSection = document.querySelector(".shows");
+
+const showsHeading = document.createElement("h2");
+showsHeading.classList.add("shows__heading");
+showsSection.appendChild(showsHeading);
+showsHeading.innerText = "Shows";
+
+for (let i = 0; i < showsArr.length; i++) {
+	const showsContainer = document.createElement("div");
+	showsContainer.classList.add("shows__container");
+	showsSection.appendChild(showsContainer);
+	// 	// showsContainer = showsArr;
+
+	const showsDateInfo = document.createElement("div");
+	showsDateInfo.classList.add("shows__container--date");
+	showsContainer.appendChild(showsDateInfo);
+
+	const showsDateHeading = document.createElement("p");
+	showsDateHeading.classList.add("shows__container--heading");
+	showsDateInfo.appendChild(showsDateHeading);
+	showsDateHeading.innerText = "Date";
+	const showsDate = document.createElement("p");
+	showsDate.classList.add("shows__container--info");
+	showsDateInfo.appendChild(showsDate);
+	showsDate.innerText = showsArr[i].date;
+
+	const showsVenueInfo = document.createElement("div");
+	showsVenueInfo.classList.add("shows__container--venue");
+	showsContainer.appendChild(showsVenueInfo);
+
+	const showsVenueHeading = document.createElement("p");
+	showsVenueHeading.classList.add("shows__container--heading");
+	showsVenueInfo.appendChild(showsVenueHeading);
+	showsVenueHeading.innerText = "Venue";
+	const showsVenue = document.createElement("p");
+	showsVenue.classList.add("shows__container--info");
+	showsVenueInfo.appendChild(showsVenue);
+	showsVenue.innerText = showsArr[i].venue;
+
+	const showsLocationInfo = document.createElement("div");
+	showsLocationInfo.classList.add("shows__container--location");
+	showsContainer.appendChild(showsLocationInfo);
+
+	const showsLocationHeading = document.createElement("p");
+	showsLocationHeading.classList.add("shows__container--heading");
+	showsLocationInfo.appendChild(showsLocationHeading);
+	showsLocationHeading.innerText = "Location";
+	const showsLocation = document.createElement("p");
+	showsLocation.classList.add("shows__container--info");
+	showsLocationInfo.appendChild(showsLocation);
+	showsLocation.innerText = showsArr[i].location;
+
+	//Buy Tickets button div
+	const buttonElWrapper = document.createElement("div");
+	buttonElWrapper.classList.add("shows__container--button");
+	showsContainer.appendChild(buttonElWrapper);
+
+	const buttonEl = document.createElement("button");
+	buttonEl.classList.add("button");
+	buttonElWrapper.appendChild(buttonEl);
+	buttonEl.innerText = "BUY TICKETS";
+}
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,6 +1,6 @@
-const commentsContainer = document.getElementsById("comments");
+//data provided on the copydeck as an array of objects
 
-const comments = [
+const commentsArr = [
 	{
 		name: "Connor Walton",
 		date: "02/17/2021",
@@ -21,36 +21,234 @@ const comments = [
 	},
 ];
 
-console.log(comments);
+//target comments section
+const commentsSection = document.querySelector(".comments--display");
 
-for (i = 0; i < comments.length; i++) {
-    
-    //create container for comments
-	let singleCommentContainer = document.createElement("section");
-	singleCommentContainer.classList.add("comments--single");
-	commentsContainer.appendChild(singleCommentContainer);
+//create containers
+const commentsContainer = document.createElement("div");
+commentsContainer.classList.add("comments__container--display");
+commentsSection.appendChild(commentsContainer);
 
+const commentsAvatarWrapper = document.createElement("div");
+commentsAvatarWrapper.classList.add("comments__container--avatar-wrapper");
+commentsContainer.appendChild(commentsAvatarWrapper);
 
-    //create element avatar
-    let commentAvatar = document.createElement("figure");
-    // Add any classes or styles
-    commentAvatar.classList.add("comment--avatar");
-    // Append the element to a parent
-    singleCommentContainer.appendChild(commentAvatar);
+const commentsContentWrapper = document.createElement("div");
+commentsContentWrapper.classList.add("comments__container--content-wrapper");
+commentsContainer.appendChild(commentsContentWrapper);
 
-    //create element name
-	let userName = document.createElement("p");
-	userName.classList.add("name--content");
-	commentsContainer.appendChild(userName);
+const commentsContentWrapperUser = document.createElement("div");
+commentsContentWrapperUser.classList.add(
+	"comments__container--content-heading"
+);
+commentsContentWrapper.appendChild(commentsContentWrapperUser);
 
-    // create element nameText
+//run iteration to display comments in array
+const displayComments = () => {
+	for (let i = 0; i < commentsArr.length; i++) {
+		const commentsUserAvatar = document.createElement("img");
+		commentsUserAvatar.classList.add("comments__container--user-avatar");
+		commentsAvatarWrapper.appendChild(commentsUserAvatar);
 
-    //create element content
-	let commentContent = document.createElement("div");
-	commentContent.classList.add("comments--content");
-	commentsContainer.appendChild(commentContent);
+		const commentsName = document.createElement("p");
+		commentsName.classList.add("comments__container--content-name");
+		commentsContentWrapper.appendChild(commentsName);
+		commentsName.innerText = commentsArr[i].name;
 
-    //create element contentText
-    let contentText = document.createElement("p");
-    contentText.innerText=comments.content[i].content;
+		const commentsDate = document.createElement("p");
+		commentsDate.classList.add("comments__container--content-date");
+		commentsContentWrapper.appendChild(commentsDate);
+		commentsDate.innerText = commentsArr[i].date;
 
+		const commentsContent = document.createElement("p");
+		commentsContent.classList.add("comments__container--content-comment");
+		commentsContentWrapper.appendChild(commentsContent);
+		commentsContent.innerText = commentsArr[i].content;
+	}
+};
+
+const commentForm = document.querySelector("#comments--form");
+// const nameInput = document.querySelector("#userName");
+// const commentInput = document.querySelector("#userComment");
+
+//adding event listener for form
+commentForm.addEventListener("submit", (event) => {
+	event.preventDefault();
+
+	//new user comment info
+	let newName = event.target.name.value;
+	// let newDate = date.toLocaleDateString();
+	let newContent = event.target.name.value;
+
+	if (newName && newContent) {
+		let newCommentObj = {
+			name: newName,
+			// date: newDate,
+			content: newContent,
+		};
+		commentsArr.push(newCommentObj);
+	}
+});
+displayComments();
+
+// 	commentsSection.innerHTML = "";
+// 	const newComment = {
+// 		name: nameInput.ariaValueMax,
+// 		content: commentInput.value,
+// 	};
+// 	commentList.unshift(newComment);
+
+// 	// commentList.forEach((element) => {
+// 	// 	displayComments(element);
+// });
+
+// const clearForm = () => {
+// 	commentForm.reset();
+// displayComments();
+// commentsName.innerText = commentsArr[0].name;
+
+// const commentsName
+
+// console.log(commentsContainer);
+
+// // // Refactor the displayTodo function so that it takes a single to do item as a parameter. Use the displayTodoRefactored function to display initial default data to the dom from the array of to do items.
+// //place the already existing comments onto the website
+// //declare the variable and class name of parent
+
+// // const todoArray = [
+// // 	"Walk the dogs",
+// // 	"Make dinner",
+// // 	"Learn jiu jitsu",
+// // 	"Write a novel",
+// // ];
+
+// // const displayTodoRefactored = (item) => {
+// // 	const todoInputValue = item.toUpperCase();
+// // 	const newTodoItem = document.createElement("li");
+// // 	newTodoItem.classList.add("todo-list__item");
+// // 	newTodoItem.innerText = todoInputValue;
+// // 	todoList.appendChild(newTodoItem);
+
+// // 	newTodoItem.addEventListener("click", handleTodoComplete);
+
+// // 	const newFaveItem = document.createElement("span");
+// // 	newFaveItem.classList.add("fave");
+// // 	newFaveItem.innerText = "❤️";
+// // 	newTodoItem.appendChild(newFaveItem);
+
+// // 	newFaveItem.addEventListener("click", handleFaving);
+// // };
+// /******************************** */
+// // Add an event handler when the to do form is submitted that will take whatever the user has typed into the input for new to do items and add display it on the dom using the displayTodoRefactored() function
+// todoForm.addEventListener("submit", (event) => {
+// 	event.preventDefault();
+// 	//   console.log(todoInput.value);
+// 	const newTodoString = todoInput.value;
+// 	displayTodoRefactored(newTodoString);
+// 	event.target.reset();
+// });
+
+// // // Iterate through the todoArray variable outside of an event handler so that the initial data from the array is displayed to the dom when the page loads.
+// for (let i = 0; i < todoArray.length; i++) {
+// 	displayTodoRefactored(todoArray[i]);
+// }
+// /**************************************************** */
+
+// 	todoForm.reset();
+// };
+
+// function renderCommentsEl() {
+// 	const commentsSection = document.querySelector("comments--shown");
+
+// 	for (let i = 0; i < comments.length; i++) {
+// 		const name = comments[i].name;
+// 		const date = comments[i].date;
+// 		const content = comments[i].content;
+// 		commentsSection.appendChild(comments);
+// 	}
+// }
+
+// renderCommentsEl();
+
+// //comments form
+// const commentForm = document.getElementById("comments--form");
+
+// //target comments in container
+// const commentsContainer = document.querySelector("#comments--display");
+
+// //function to generate HTML
+// const renderComments = () => {
+// 	//create elements and set classes to each
+
+// }
+
+// //create button function
+// // buttonEl = document.querySelector(".")
+// function displayComments(commentsArr) {
+// 	console.log("test for comments section");
+// 	for (i = 0; i < commentsArr.length; i++) {
+// 		console.log("test within loop");
+// 		// const commentsContainer = document.createElement("div");
+// 		// commentsContainer.classList.add("comments__container");
+// 		// console.log();
+// 		const commentsHeading = document.createElement("h1");
+// 		commentsHeading.classList.add("comments__heading");
+// 		commentsHeading.innerText = "Join the Conversation";
+// 		commentsContainer.appendChild(commentsHeading); //parent, appendchild, child
+// 		const commentsName = document.createElement("h2");
+// 		const inputEl = document.querySelector(".input-field");
+// 		inputEl.addEventListener("input", (event) => {
+// 			console.log("input event object: ", event);
+// 			console.log("event.target: ", event.target);
+// 			console.log("event.target.value: ", event.target.value);
+// 		});
+// 		commentsName.classList.add("comments__name");
+// 		commentsName.innerText = commentsArr[i].name;
+// 		const commentsContent = document.createElement("h2");
+// 		commentsContent.classList.add("comments__content");
+// 		commentsContent.innerText = commentsArr[i].content;
+
+// 		//add to the whole section
+// 		commentsSection.appendChild(commentsContainer);
+// 	}
+// }
+// displayComments(comments[i]);
+
+// // const inputEl = document.querySelector(".input-field");
+// // inputEl.addEventListener("input", (event) => {
+// // 	console.log("input event object: ", event);
+// // 	console.log("event.target: ", event.target);
+// // 	console.log("event.target.value: ", event.target.value);
+// // });
+// // // //create container for comments
+// // // let singleCommentContainer = document.createElement("section");
+// // // singleCommentContainer.classList.add("comments--single");
+// // // commentsContainer.appendChild(singleCommentContainer);
+
+// // // //create element avatar
+// // // let commentAvatar = document.createElement("figure");
+// // // // Add any classes or styles
+// // // commentAvatar.classList.add("comment--avatar");
+// // // // Append the element to a parent
+// // // singleCommentContainer.appendChild(commentAvatar);
+
+// // // //create element name
+// // // let userName = document.createElement("p");
+// // // userName.classList.add("name--content");
+// // // commentsContainer.appendChild(userName);
+
+// // // // create element nameText
+
+// // // //create element content
+// // // let commentContent = document.createElement("div");
+// // // commentContent.classList.add("comments--content");
+// // // commentsContainer.appendChild(commentContent);
+
+// // // //create element contentText
+// // // let contentText = document.createElement("p");
+// // // contentText.innerText = comments.content[i].content;
+
+// //iterate through the comments array variable outside of an event handler so that the initial data from the array is displayed.
+// for (let i = 0; i < comments.length; i++) {
+// 	commentsRefactored(comments[i]);
+// }
